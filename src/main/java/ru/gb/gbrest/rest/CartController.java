175104b9bad09370b099ab1429ec82f1803590a8
KuznetsovM <kuznetcoff@mail.ru>
2021-12-27 23:10:57 +0300

    lesson10 Добавил микросервис заказа.

--- src/main/java/ru/gb/gbrest/rest/CartController.java
+++ src/main/java/ru/gb/gbrest/rest/CartController.java
@@ -1,39 +1,56 @@
 package ru.gb.gbrest.rest;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.gb.gbrest.dto.CartDto;
-import ru.gb.gbrest.service.CartService;
+import ru.gb.gbrest.service.feign.CartDtoApi;
+import ru.gb.gbrest.service.resttemplate.ManufacturerDtoApiV2;
 
-import java.net.URI;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/cart")
 public class CartController {
-    private final CartService cartService;
+//    private final CartService cartService;
+
+    private final CartDtoApi cartDtoApi;
+
+
+    @PostMapping
+    public CartDto handlePost(@RequestBody CartDto cartDto) {
+        return cartDtoApi.handlePost(cartDto);
+    }
 
     @GetMapping
     public List<CartDto> getProductList() {
-        return cartService.findAll();
-    }
-
-    @PostMapping
-    public ResponseEntity<?> handlePost(@RequestBody CartDto cartDto) {
-        CartDto savedCart = cartService.save(cartDto);
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setLocation(URI.create("/cart/" + savedCart.getId()));
-        return new ResponseEntity<>(httpHeaders, HttpStatus.CREATED);
+        return cartDtoApi.getProductList();
     }
 
     @DeleteMapping("/{cartId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteById(@PathVariable("cartId") Long id) {
-        cartService.deleteById(id);
+        cartDtoApi.deleteById(id);
     }
+
+//    @GetMapping
+//    public List<CartDto> getProductList() {
+//        return cartService.findAll();
+//    }
+
+//    @PostMapping
+//    public ResponseEntity<?> handlePost(@RequestBody CartDto cartDto) {
+//        CartDto savedCart = cartService.save(cartDto);
+//        HttpHeaders httpHeaders = new HttpHeaders();
+//        httpHeaders.setLocation(URI.create("/cart/" + savedCart.getId()));
+//        return new ResponseEntity<>(httpHeaders, HttpStatus.CREATED);
+//    }
+
+//    @DeleteMapping("/{cartId}")
+//    @ResponseStatus(HttpStatus.NO_CONTENT)
+//    public void deleteById(@PathVariable("cartId") Long id) {
+//        cartService.deleteById(id);
+//    }
 
 }
--- src/main/java/ru/gb/gbrest/service/feign/CartDtoApi.java
+++ src/main/java/ru/gb/gbrest/service/feign/CartDtoApi.java
@@ -1,7 +1,7 @@
 package ru.gb.gbrest.service.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.gb.gbrest.dto.CartDto;
 
@@ -10,16 +10,17 @@ import java.util.List;
 @FeignClient(url = "localhost:8457/cart", value = "cartDtoApi")
 public interface CartDtoApi {
 
+    @PostMapping(produces = "application/json;charset=UTF-8",
+            consumes = "application/json;charset=UTF-8")
+    CartDto handlePost(@RequestBody CartDto cartDto);
 
     @GetMapping(produces = "application/json;charset=UTF-8",
             consumes = "application/json;charset=UTF-8")
     List<CartDto> getProductList();
 
-    @GetMapping(produces = "application/json;charset=UTF-8",
+    @DeleteMapping(value = "/{cartId}", produces = "application/json;charset=UTF-8",
             consumes = "application/json;charset=UTF-8")
-    ResponseEntity<?> handlePost(@RequestBody CartDto cartDto);
-
-    @DeleteMapping("/{cartId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     void deleteById(@PathVariable("cartId") Long id);
 
 }
